dac57d60e68b2bc21323850190d691f873e9ef69
kewldan <kewldanil1@gmail.com>
2024-09-30 21:03:57 +0300

    fixes

--- web/src/components/topbar.tsx
+++ web/src/components/topbar.tsx
@@ -2,24 +2,27 @@
 
 import React from "react";
 import ChildSelector from "@/components/child-selector";
-import {RefreshCcw, Settings} from "lucide-react";
+import {Settings, Share2} from "lucide-react";
 import {Button} from "@/components/ui/button";
 import ACSDrawer from "@/components/acs-drawer";
 import BalanceDrawer from "@/components/balance-drawer";
 import Link from "next/link";
-import {useRouter} from "next/navigation";
+import {useUtils} from "@telegram-apps/sdk-react";
 
 export default function TopBar() {
-    const router = useRouter();
+    const utils = useUtils(true);
 
     return (
         <div className="sticky top-0 h-12 px-2 flex justify-between w-full bg-background">
             <ChildSelector/>
             <div>
                 <Button size="icon" variant="ghost" onClick={() => {
-                    router.refresh();
+                    if (!utils)
+                        return;
+
+                    utils.shareURL('https://t.me/ednevnik_spb_bot', '👋 Ей! Посмотри, я нашел крутейший дневник с кучей фишек и крутых функций! 😎')
                 }}>
-                    <RefreshCcw/>
+                    <Share2/>
                 </Button>
                 <ACSDrawer/>
                 <BalanceDrawer/>
